092256a48cf9406593dc2cea0df7026a643a128d
Your Name <you@example.com>
2019-08-27 20:56:32 +0530

    @Test annotation is implemented

--- src/test/java/testng/testing/TestngPractice1.java
+++ src/test/java/testng/testing/TestngPractice1.java
@@ -1,5 +1,28 @@
 package testng.testing;
 
+import org.testng.annotations.Test;
+
 public class TestngPractice1 {
+
+	@Test
+	public void openBrowser() {
+	System.out.println("open browser");
+	}
+	@Test
+	public void openUrl() {
+	System.out.println("open url");
+	}
+	@Test
+	public void searchProduct() {
+	System.out.println("search iphone");
+	}
+	@Test
+	public void verifyProduct() {
+	System.out.println("verify product");
+	}
+	@Test
+	public void closeBrowser() {
+	System.out.println("close browser");
+	}
 
 }
